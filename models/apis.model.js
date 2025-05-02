63f1ae0bf05550594a7d610f32d563eaeb70bdc8
manuelgcl25 <mgcaldasl@gmail.com>
2025-05-02 15:19:23 +0100

    Get all articles + test suites

--- models/apis.model.js
+++ models/apis.model.js
@@ -20,4 +20,25 @@ async function selectArticleById(articleId) {
   return rows;
 }
 
-module.exports = { selectTopics, selectArticleById };
+async function selectArticles() {
+  const { rows } = await db.query(`
+    SELECT articles.author, 
+    articles.title, 
+    articles.article_id, 
+    articles.topic, 
+    articles.created_at, 
+    articles.votes, 
+    articles.article_img_url, 
+    COUNT(comments.comment_id)::INT AS comment_count 
+    FROM articles
+    JOIN
+    comments ON articles.article_id = comments.article_id
+    GROUP BY 
+    articles.article_id
+    ORDER BY created_at DESC;
+    `);
+
+  return rows;
+}
+
+module.exports = { selectTopics, selectArticleById, selectArticles };
